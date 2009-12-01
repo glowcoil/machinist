c5cda00822e480515bed64121581bc76ca2a37de
Micah <wyndclaw@gmail.com>
2009-11-30 21:22:52 -0700

    trying new keyboard system and changed actor

--- machinist.js
+++ machinist.js
@@ -3,8 +3,7 @@ function game(canvas, scene) {
     game.canvas = document.getElementById(canvas);
     game.ctx = game.canvas.getContext('2d');
     game.scene = scene;
-
-    game.scene.init();
+    
     setInterval(function() { game.scene.update(); }, 1000 / 60);
     setInterval(function() { game.scene.draw(); }, 1000 / 60);
   }
@@ -12,27 +11,12 @@ function game(canvas, scene) {
 
 game.changeScene = function(scene) {
   game.scene = scene;
-  game.scene.init();
 }
 
-function Actor(init, update, draw) {
-  this.init = init || function() {};
-  this.update = update || function() {};
-  this.draw = draw || function() {
-    this.sprite.draw(this.x, this.y);
-  };
-}
-
-/*
- *  Scene takes a list of Actors
- */
+// Scene takes a list of actors
 function Scene() {
   this.actors = [];
   for (a in arguments) this.actors.push(arguments[a]);
-}
-
-Scene.prototype.init = function() {
-  for (a in this.actors) this.actors[a].init();
 }
 
 Scene.prototype.update = function() {
@@ -41,5 +25,8 @@ Scene.prototype.update = function() {
 
 Scene.prototype.draw = function() {
   game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
-  for (a in this.actors) this.actors[a].draw();
+  for (a in this.actors) {
+    if (this.actors[a].draw) this.actors[a].draw();
+    else this.actors[a].sprite.draw(this.actors[a].x, this.actors[a].y);
+  }
 }
