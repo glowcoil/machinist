263dae53a3c24175e24cff732a62207b186a13d8
Micah <wyndclaw@gmail.com>
2009-12-05 11:30:30 -0700

    now works in firefox

--- machinist.js
+++ machinist.js
@@ -11,22 +11,27 @@ function game(canvas, scene) {
 
 game.changeScene = function(scene) {
   game.scene = scene;
+  for (k in key.used) key.used[k] = false;
 }
 
 // Scene takes a list of actors
 function Scene() {
   this.actors = [];
-  for (a in arguments) this.actors.push(arguments[a]);
+  for (var a = 0, l = arguments.length; a < l; ++a) this.actors[a] = arguments[a];
+  for (var a in this.actors) {
+    if (!this.actors[a].draw) this.actors[a].draw = function() { this.sprite.draw(this.x, this.y); };
+  }
 }
 
 Scene.prototype.update = function() {
-  for (a in this.actors) this.actors[a].update();
+  for (a in this.actors) {
+    if (this.actors[a].update) this.actors[a].update();
+  }
 }
 
 Scene.prototype.draw = function() {
   game.ctx.clearRect(0, 0, game.canvas.width, game.canvas.height);
   for (a in this.actors) {
-    if (this.actors[a].draw) this.actors[a].draw();
-    else this.actors[a].sprite.draw(this.actors[a].x, this.actors[a].y);
+    this.actors[a].draw();
   }
 }
