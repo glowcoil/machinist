1e68ae9e87f280303d1e88142910e8f90f8396f0
Micah Johnston <wyndclaw@gmail.com>
2010-04-17 18:35:50 -0600

    removing useless delay

--- test.js
+++ test.js
@@ -1,4 +1,3 @@
-setTimeout(function() {
 guy = {
   sprite: new Sprite("res/test.png"),
   sound: new Sound("res/test.ogg"),
@@ -9,6 +8,8 @@ guy = {
     if (key(keys.down)) this.y += 4;
     if (key(keys.left)) this.x -= 4;
     if (key(keys.right)) this.x += 4;
+    
+    if (key(keys.space)) this.sound.play();
   },
   draw: function() {
     this.sprite.draw(this.x, this.y);
@@ -18,4 +19,3 @@ guy = {
 level1 = new Scene(guy);
 
 game("canvas", level1);
-}, 2);
